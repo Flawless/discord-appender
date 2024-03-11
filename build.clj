216470595e3d9e7eac2c24146964b4653fe3d7b3
flawless <alushanov92@gmail.com>
2024-03-11 14:24:51 +0400

    Add license and improve build process

--- build.clj
+++ build.clj
@@ -2,28 +2,37 @@
   (:require [clojure.string :as str]
             [clojure.tools.build.api :as b]))
 
-(def lib 'org.last-try/discord-appender)
+(def lib 'io.github.flawless/discord-appender)
 (def dirty? (not (str/blank? (b/git-process {:git-args "status --porcelain"}))))
 (def version (cond-> (or (b/git-process {:git-args "describe --tags --abbrev=0"})
                          (b/git-process {:git-args "rev-parse --short HEAD"}))
                dirty? (str "-dirty")))
 (def class-dir "target/classes")
-(def basis (b/create-basis {:project "deps.edn"}))
+(def basis (delay (b/create-basis {:project "deps.edn"})))
 (def jar-base (format "target/%s.jar" (name lib)))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
 
 (defn clean [_]
-  (b/delete {:path "target"}))
+  (b/delete {:path "target"})
+  (b/delete {:path "pom.xml"})
+  (b/delete {:path "pom.properties"}))
 
 (defn jar [_]
-  (println "building" version)
-  (b/write-pom {:class-dir class-dir
-                :lib lib
-                :version version
-                :basis basis
-                :src-dirs ["src"]})
+  (println :building version)
   (b/copy-dir {:src-dirs ["src" "resources"]
                :target-dir class-dir})
+  (b/write-pom {#_#_:class-dir class-dir
+                :target "."
+                :lib lib
+                :version version
+                :basis @basis
+                :src-dirs ["src"]
+                :pom-data [[:licenses
+                            [:license
+                             [:name "Apache License 2.0"]
+                             [:url "http://www.apache.org/licenses/"]
+                             [:distribution "repo"]]]]})
   (b/jar {:class-dir class-dir
           :jar-file jar-file})
-  (b/copy-file {:src jar-file :target jar-base}))
+  (b/copy-file {:src jar-file :target jar-base})
+  (println :built jar-file jar-base))
