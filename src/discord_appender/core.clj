72b1d60499a364f734a0355f3f81ff97e6d6657a
flawless <alushanov92@gmail.com>
2024-04-29 10:15:40 +0400

    Allow level filtering

--- src/discord_appender/core.clj
+++ src/discord_appender/core.clj
@@ -1,10 +1,11 @@
 (ns discord-appender.core
-  (:require [clj-http.client :as client]
-            [clojure.java.io :as io]
-            [clojure.string :as str]))
+  (:require
+   [clj-http.client :as client]
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [taoensso.timbre :as timbre]))
 
-(defn- trunc
-  [s n]
+(defn- trunc [s n]
   (subs s 0 (min (count s) n)))
 
 (defn discord-appender
@@ -12,24 +13,25 @@
     `hook-id` - discord webhook id
     `token`   - discord hook token
   https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks"
-  [& [{:keys [token hook-id username]
+  [& [{:keys [token hook-id username min-level]
        :or {username "good bot"}}]]
   (let [webhook-url (format "https://discord.com/api/webhooks/%s/%s" hook-id token)]
     {:enabled?   true
      :async?     true
      :output-fn :inherit
      :fn (fn [data]
-           (let [[first-line :as message] (-> data :output_ force str/split-lines)
-                 n 2000]
-             (try
-               (client/post webhook-url {:form-params {:content (trunc first-line n)
-                                                       :username username}})
-               (when (< (count message) 2000)
-                 (client/post
-                   webhook-url {:multipart
-                                [{:name "username" :content username}
-                                 {:name "content.txt" :content (-> (.getBytes (str/join "\n" message)
-                                                                     "UTF-8")
-                                                                 io/input-stream)}]}))
-               (catch Exception e
-                 (println :error e "can't append to discord")))))}))
+           (when (timbre/level>= (:level data) min-level)
+             (let [[first-line :as message] (-> data :output_ force str/split-lines)
+                   n 2000]
+               (try
+                 (client/post webhook-url {:form-params {:content (trunc first-line n)
+                                                         :username username}})
+                 (when (< (count message) 2000)
+                   (client/post
+                     webhook-url {:multipart
+                                  [{:name "username" :content username}
+                                   {:name "content.txt" :content (-> (.getBytes (str/join "\n" message)
+                                                                       "UTF-8")
+                                                                   io/input-stream)}]}))
+                 (catch Exception e
+                   (println :error e "can't append to discord"))))))}))
